071bddc536626f8387786299ef6231d30990afc1
Abdul6464516 <raz6464516@gmail.com>
2024-10-21 08:34:32 +0500

    delete button was added with each li to remove its content

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ const button = document.querySelector("#btn");
 
 function todoHandler(event){
     event.preventDefault();
+    const div = document.querySelector("#div");
     
     const input = document.querySelector("#input");
     const inputValue = input.value;
+    if(inputValue.trim()===""){
+        const span = document.createElement("span");
+        span.innerText = "Input must not be empty";
+        span.classList.add("spanColor");
+        div.appendChild(span);
+        return 
+    }
     const ul = document.getElementById("ul");
 
      let li = document.createElement("li")
+     let deleteButton = document.createElement("button");
+     const buttonText = document.createTextNode('Click Me');
+     deleteButton.appendChild(buttonText)
+     console.log(deleteButton)
+     
      li.innerText = inputValue;
-     ul.appendChild(li);
+     li.appendChild(deleteButton)
+     ul.appendChild(li,deleteButton);
+     console.log(li)
      input.value = " "
 }
 button.addEventListener("click",todoHandler)
